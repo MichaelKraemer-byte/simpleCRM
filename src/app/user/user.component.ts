d7c4779405edbc1c310117f3694e9b80e0a0ce35
Michael Krämer <info.m.kraemer@gmail.com>
2024-10-26 22:15:56 +0200

    render users in list

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import {MatCardModule} from '@angular/material/card';
+import { collection, Firestore, getDoc, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { User } from '../class/user.class';
+
 
 
 
@@ -16,14 +20,28 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatTooltipModule,
     MatDialogModule,
-    MatCardModule
+    MatCardModule,
+    CommonModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+
+  allUsers: User[] = [];
   
-  constructor(public dialog: MatDialog){
+  constructor(public dialog: MatDialog, private firestore: Firestore){
+  }
+
+  ngOnInit(): void {
+    const userCollection = collection(this.firestore, 'users');
+
+    collectionData(userCollection)
+    .subscribe((changes: any) => {
+        console.log('Received changes', changes);  
+        this.allUsers = changes;
+    })
   }
 
   openDialog(){
